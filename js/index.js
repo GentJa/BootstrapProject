ef6196c91541187092506e7aceb1683b0948c1d6
Gent Jashari <gentjashari1997@gmail.com>
2021-09-16 16:54:35 +0200

    -

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbarContainer").addClass("sticky");
+    } else {
+      $(".navbarContainer").removeClass("sticky");
+    }
+  });
+});
+
+$(".menu-btn").click(function () {
+  $(".navbarContainer .menu").toggleClass("active");
+  $(".menu-btn i").toggleClass("active");
+});
+
